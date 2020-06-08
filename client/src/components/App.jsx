18a2439596950f1cf127491372540bb1042dee25
Joel Kapp <joel.g.kapp@gmail.com>
2020-06-08 13:41:12 -0600

    Used to react-router to route between different pages

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import Home from './Home.jsx';
+import Shipments from './Shipments.jsx';
+import CarpetManifest from './CarpetManifest.jsx';
 
 const App = () => (
-  <h1>Hello from App!</h1>
+  <Router>
+    <Switch>
+      <Route path="/shipments/:shipmentId/carpet">
+        <CarpetManifest />
+      </Route>
+      <Route path="/shipments">
+        <Shipments />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="*">
+        <Redirect to="/" />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
